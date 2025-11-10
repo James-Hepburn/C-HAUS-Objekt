7c5b40098b47f65ea3a1e9e79bf8a754d07ad381
James Hepburn <jhepb059@uottawa.ca>
2025-11-10 14:59:40 -0700

    Work page finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./Landing"; 
 import Home from "./Home";
+import Work from "./Work";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/work" element={<Work />} />
       </Routes>
     </Router>
   );
